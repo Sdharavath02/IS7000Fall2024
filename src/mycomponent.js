f4ebc029292894f16d3d01f3218ee397f7523019
Sdharavath02 <sdharavath02@indianatech.net>
2024-11-11 17:30:07 -0500

    Create mycomponent.js

---
+++ src/mycomponent.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function MyComponent() {
+  return (
+    <div>
+      <h1>Welcome to My Component</h1>
+      <p>This is your first frontend component!</p>
+    </div>
+  );
+}
+
+export default MyComponent;
